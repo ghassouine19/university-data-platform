22c64f816e833a814b1fdf881c20f2a6349ddf45
ELAYADI Younes <younesselayadi05@gmail.com>
2026-07-20 22:04:05 +0200

    modification de structure de metadata.py

--- jobs/common/metadata.py
+++ jobs/common/metadata.py
@@ -1,6 +1,4 @@
 # jobs/common/metadata.py
-from array import ArrayType
-
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType, ArrayType
 
 # ==============================================================================
@@ -128,7 +126,7 @@ BRONZE_ORCID_INPUT_SCHEMA = StructType([
     # 1. Identifiant Unique ORCID (orcid-identifier -> path)
     StructField("orcid-identifier", StructType([
         StructField("path", StringType(), True),
-        StructField("uri", StructType(), True)
+        StructField("uri", StringType(), True)
     ]), True),
 
     # 2. Données Personnelles du Chercheur (person -> name, emails, biography, addresses)
@@ -175,3 +173,15 @@ BRONZE_ORCID_INPUT_SCHEMA = StructType([
         ]), True)
     ]), True)
 ])
+
+
+#les clè métier
+BUSINESS_KEYS = {
+
+    "research_publications": ["doi"],
+
+    "faculty_profiles": ["orcid"],
+
+    "documents_registry": ["payload_checksum"]
+
+}
